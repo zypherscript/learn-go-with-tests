62a338f08054e403ba5145e697bae75d15bb8866
zypherscript <kitti.kthp@gmail.com>
2023-06-30 15:58:29 +0700

    structs, methods and interfaces

---
+++ structs/shapes.go
@@ -0,0 +1,39 @@
+package structs
+
+import (
+	"math"
+)
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
