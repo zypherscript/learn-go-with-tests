4791ca52d64caf4771e37187d94184277a06a3c1
zypherscript <kitti.kthp@gmail.com>
2023-07-05 14:29:04 +0700

    select v1

---
+++ select/racer.go
@@ -0,0 +1,47 @@
+package racer
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, err error) {
+	// aDuration := measureResponseTime(slowURL)
+	// bDuration := measureResponseTime(fastURL)
+
+	// if aDuration < bDuration {
+	// 	return slowURL
+	// }
+	// return fastURL
+
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func measureResponseTime(url string) time.Duration {
+	startA := time.Now()
+	http.Get(url)
+	return time.Since(startA)
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
---
+++ select/racer_test.go
@@ -0,0 +1,53 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("return winner", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		//By prefixing a function call with defer it will now call that function at the end of the containing function.
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+		// slowServer.Close()
+		// fastServer.Close()
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
