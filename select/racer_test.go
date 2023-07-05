a7ed5e4a96617c3850db691cf343c7b7e9b759d6
zypherscript <kitti.kthp@gmail.com>
2023-07-05 14:33:21 +0700

    refactor test

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,11 +8,10 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	t.Run("return winner", func(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
 		slowServer := makeDelayedServer(20 * time.Millisecond)
 		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-		//By prefixing a function call with defer it will now call that function at the end of the containing function.
 		defer slowServer.Close()
 		defer fastServer.Close()
 
@@ -20,24 +19,23 @@ func TestRacer(t *testing.T) {
 		fastURL := fastServer.URL
 
 		want := fastURL
-		got, _ := Racer(slowURL, fastURL)
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
-
-		// slowServer.Close()
-		// fastServer.Close()
 	})
 
-	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+	t.Run("returns an error if a server doesn't respond within the specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
