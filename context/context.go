ac3df039b078686a76a6b04e4b8d100d0d4b2593
zypherscript <kitti.kthp@gmail.com>
2023-07-08 23:39:50 +0700

    context v1

---
+++ context/context.go
@@ -0,0 +1,30 @@
+package context1
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
+	}
+}
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
