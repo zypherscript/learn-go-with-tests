cbe4e9f29426ad06ff7b893d91da434122bfc528
zypherscript <kitti.kthp@gmail.com>
2023-07-11 22:37:28 +0700

    tmp: math

---
+++ math/v2/clockface.go
@@ -0,0 +1,34 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A Point represents a two dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+// func main() {
+// 	fmt.Println(secondsinradians())
+// }
+
+// func zero() float64 {
+// 	return 0.0
+// }
+
+// func secondsinradians() float64 {
+// 	return (math.Pi / (30 / (float64(zero()))))
+// }
---
+++ math/v5/clockface_acceptance_test.go
@@ -0,0 +1,1 @@
+package clockface_test
