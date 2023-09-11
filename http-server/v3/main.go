637d7c134f76101a792d27b5000f45b16a7881d7
zypherscript <kitti.kthp@gmail.com>
2023-09-11 16:16:57 +0700

    http server 2

---
+++ http-server/v3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5000", server))
+}
